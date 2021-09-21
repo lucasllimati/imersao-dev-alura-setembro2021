98f316452ab1b3299a58b9417a5db0fe4ac34c73
lucasllimati <lucasllimati@gmail.com>
2021-09-21 13:13:21 -0300

    Development

--- Aula 6 - Tabela de classificação - Setembro2021/dist/script.js
+++ Aula 6 - Tabela de classificação - Setembro2021/dist/script.js
@@ -1,5 +1,5 @@
 var rafa = {
-    name:"Rafa",
+    nome:"Rafa",
     vitorias: 2,
     empates: 1,
     derrotas: 1,
@@ -7,10 +7,26 @@ var rafa = {
 };
 
 var paulo = {
-    name:"Paulo",
+    nome:"Paulo",
     vitorias: 1,
     empates: 1,
     derrotas: 2,
+    pontos: 0
+};
+
+var gui = {
+    nome:"Gui",
+    vitorias: 1,
+    empates: 2,
+    derrotas: 1,
+    pontos: 0
+};
+
+var lucas = {
+    nome:"Lucas",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
     pontos: 0
 };
 
@@ -21,3 +37,46 @@ function calculaPontos(jogador) {
 
 rafa.pontos = calculaPontos(rafa);
 paulo.pontos = calculaPontos(paulo);
+gui.pontos = calculaPontos(gui);
+lucas.pontos = calculaPontos(lucas);
+
+var jogadores = [rafa, paulo, gui, lucas];
+
+function exibeJogadoresNaTela(jogadores) {
+    var elemento = ""
+    for (var i = 0; i < jogadores.length; i++) {
+        elemento += "<tr><td>" + jogadores[i].nome+ "</td>"
+        elemento += "<td>" + jogadores[i].vitorias + "</td>"
+        elemento += "<td>" + jogadores[i].empates + "</td>"
+        elemento += "<td>" + jogadores[i].derrotas + "</td>"
+        elemento += "<td>" + jogadores[i].pontos + "</td>"
+        elemento += "<td><button onClick='adicionarVitoria("+ i +")'>Vitória</button></td>"
+        elemento += "<td><button onClick='adicionarEmpate("+ i +")'>Empate</button></td>"
+        elemento += "<td><button onClick='adicionarDerrota("+ i +")'>Derrota</button></td>"
+        elemento += "</tr>"
+    }
+    var tabelaJogadores = document.getElementById("tabelaJogadores")
+    tabelaJogadores.innerHTML = elemento
+}
+
+exibeJogadoresNaTela(jogadores);
+
+function adicionarVitoria(i) {
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i) {
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibeJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i) {
+    var jogador = jogadores[i]
+    jogador.derrotas++
+    exibeJogadoresNaTela(jogadores)
+}
