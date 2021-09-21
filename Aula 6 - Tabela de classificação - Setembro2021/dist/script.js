fdf5880058933b46838517b420878c952611adc1
lucasllimati <lucasllimati@gmail.com>
2021-09-21 12:48:30 -0300

    Development

--- Aula 6 - Tabela de classificação - Setembro2021/dist/script.js
+++ Aula 6 - Tabela de classificação - Setembro2021/dist/script.js
@@ -1,1 +1,29 @@
-console.log();
+var rafa = {
+    name:"Rafa",
+    vitorias: 2,
+    empates: 1,
+    derrotas: 1,
+    pontos: 0
+};
+
+var paulo = {
+    name:"Paulo",
+    vitorias: 1,
+    empates: 1,
+    derrotas: 2,
+    pontos: 0
+};
+
+// console.log(rafa.vitorias)
+// console.log(paulo.empates)
+
+console.log(rafa)
+var resultadoDosPontos
+calculaPontos(rafa);
+
+
+function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias * 3) + jogador.empates;
+    console.log(pontos);
+}
+console.log(rafa)
